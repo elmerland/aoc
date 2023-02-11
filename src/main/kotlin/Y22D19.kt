5e00a03233905dba3ccbba1ab3012a603021adc3
Elmer Landaverde <elmerlandaverde@gmail.com>
2023-02-10 23:17:20 -0600

    Day 19 solution

--- src/main/kotlin/Y22D19.kt
+++ src/main/kotlin/Y22D19.kt
@@ -1,165 +1,250 @@
+import kotlin.math.ceil
+
 class Y22D19 : Puzzle(22, 19) {
+
     override fun part1(): String {
-        val re = Regex("^Blueprint (\\d+): .* (\\d+) .* (\\d+) .* (\\d+) .* (\\d+) .* (\\d+) .* (\\d+) .*\$")
-        val (bluePrint, oreRobot, clayRobot, obRobotOre, obRobotClay, geoRobotOre, geoRobotOb) = re.find(input[0])!!.destructured
-        val x = maxGeodesForBlueprint(
-            listOf(
+        val re = Regex("^Blueprint (\\d+): " +
+                "Each ore robot costs (\\d+) ore. " +
+                "Each clay robot costs (\\d+) ore. " +
+                "Each obsidian robot costs (\\d+) ore and (\\d+) clay. " +
+                "Each geode robot costs (\\d+) ore and (\\d+) obsidian.\$")
+        val blueprints = input.map {
+            val (
+                bpId,
                 oreRobot,
                 clayRobot,
-                obRobotOre,
-                obRobotClay,
-                geoRobotOre,
-                geoRobotOb
-            ).map { num -> num.toInt() })
-        println("RESULT $x")
-//        val blueprints = input[0].mapIndexed { index, s ->
-//            val (bluePrint, oreRobot, clayRobot, obRobotOre, obRobotClay, geoRobotOre, geoRobotOb) = re.find(s)!!.destructured
-////            if (index > 0) { throw IllegalStateException() }
-//            bluePrint.toInt() to maxGeodesForBlueprint(
-//                listOf(
-//                    oreRobot,
-//                    clayRobot,
-//                    obRobotOre,
-//                    obRobotClay,
-//                    geoRobotOre,
-//                    geoRobotOb
-//                ).map { num -> num.toInt() })
-//        }
+                obsidianRobotOre,
+                obsidianRobotClay,
+                geodeRobotOre,
+                geodeRobotObsidian,
+            ) = re.find(it)!!.destructured
+            BluePrint(
+                bpId.toInt(),
+                oreRobot.toInt(),
+                clayRobot.toInt(),
+                obsidianRobotOre.toInt() to obsidianRobotClay.toInt(),
+                geodeRobotOre.toInt() to geodeRobotObsidian.toInt(),
+            )
+        }
 
-//        println(blueprints.joinToString("\n"))
-        TODO("Not yet implemented")
+        return blueprints.sumOf { it.id * findMaxGeodes(it, 24) }.toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val re = Regex("^Blueprint (\\d+): " +
+                "Each ore robot costs (\\d+) ore. " +
+                "Each clay robot costs (\\d+) ore. " +
+                "Each obsidian robot costs (\\d+) ore and (\\d+) clay. " +
+                "Each geode robot costs (\\d+) ore and (\\d+) obsidian.\$")
+        val blueprints = input.map {
+            val (
+                bpId,
+                oreRobot,
+                clayRobot,
+                obsidianRobotOre,
+                obsidianRobotClay,
+                geodeRobotOre,
+                geodeRobotObsidian,
+            ) = re.find(it)!!.destructured
+            BluePrint(
+                bpId.toInt(),
+                oreRobot.toInt(),
+                clayRobot.toInt(),
+                obsidianRobotOre.toInt() to obsidianRobotClay.toInt(),
+                geodeRobotOre.toInt() to geodeRobotObsidian.toInt(),
+            )
+        }.take(3)
+
+        return blueprints.map { findMaxGeodes(it, 32) }.reduce { acc, i -> acc * i }.toString()
     }
 
-    private fun maxGeodesForBlueprint(blueprint: List<Int>): Int {
-
-        println("blueprint config $blueprint")
-        val oreRobotOreCost = blueprint[0]
-        val clayRobotOreCost = blueprint[1]
-        val obsidianRobotOreCost = blueprint[2]
-        val obsidianRobotClayCost = blueprint[3]
-        val geodeRobotOreCost = blueprint[4]
-        val geodeRobotObsidianCost = blueprint[5]
-
-
-        data class Store(
-            val time: Int = 1,
-            val ore: Int = 0,
-            val clay: Int = 0,
-            val obsidian: Int = 0,
-            val geodes: Int = 0,
-            val oreRobots: Int = 1,
-            val clayRobots: Int = 0,
-            val obsidianRobots: Int = 0,
-            val geoRobots: Int = 0,
-        ) {
-            fun canAfford(robot: Robot): Boolean {
-                return when (robot) {
-                    Robot.ORE -> ore >= oreRobotOreCost
-                    Robot.CLAY -> ore >= clayRobotOreCost
-                    Robot.OBSIDIAN -> ore >= obsidianRobotOreCost && clay >= obsidianRobotClayCost
-                    Robot.GEODE -> ore >= geodeRobotOreCost && obsidian >= geodeRobotObsidianCost
+    private fun findMaxGeodes(bp: BluePrint, timeLimit: Int): Int {
+        val debug = false
+        fun dp(t: Int, store: Store, robots: RobotFactory): Int {
+            val indent = "\t".repeat(t-1)
+            if (debug) println("$indent> dp: $t, $store, $robots")
+            if (t >= timeLimit) {
+                if (t > timeLimit) {
+                    throw IllegalStateException("Time exceeded maximum $t")
                 }
+                val updatedStore = robots.mine(store)
+                if (debug) println("$indent> RESULT $t, $updatedStore, $robots")
+                return updatedStore.geodes
             }
 
-            fun canAffordAny(): Boolean {
-                return Robot.values().any { canAfford(it) }
+            val makeRobotMaxScore = Robot.values().mapNotNull { robotType ->
+                robots.timeToAfford(robotType, store, bp)?.let { robotType to it }
             }
-
-            fun buyRobot(robot: Robot): Store {
-                return when (robot) {
-                    Robot.ORE -> this.copy(ore = ore - oreRobotOreCost, oreRobots = oreRobots + 1)
-                    Robot.CLAY -> this.copy(ore = ore - clayRobotOreCost, clayRobots = clayRobots + 1)
-                    Robot.OBSIDIAN -> this.copy(
-                        ore = ore - obsidianRobotOreCost,
-                        clay = clay - obsidianRobotClayCost,
-                        obsidianRobots = obsidianRobots + 1
-                    )
-
-                    Robot.GEODE -> this.copy(
-                        ore = ore - geodeRobotOreCost,
-                        obsidian = obsidian - geodeRobotObsidianCost,
-                        geoRobots = geoRobots + 1
-                    )
+                .sortedBy { (robotType, _) -> robotType.ordinal }
+                // Skip any robots that would take us past the time limit
+                .filter { (_, timeToAfford) -> (t + timeToAfford) < timeLimit }
+                // Skip robots that don't make sense to prioritize
+                .filter { (robotType, _) ->
+                    when (robotType) {
+                        Robot.ORE -> {
+                            // Don't make new ore robots if we have enough robots to make either a clay, obsidian, or geode robot
+                            val maxOreRobots = maxOf(bp.clayRobot, bp.obsidianRobot.first, bp.geodeRobot.first)
+                            if (robots.ore >= maxOreRobots) {
+                                if (debug) println("$indent> skip making ore robot")
+                                false
+                            } else {
+                                true
+                            }
+                        }
+                        Robot.CLAY -> {
+                            // Don't make new clay robots if we have enough robots to make obsidian
+                            if (robots.clay >= bp.obsidianRobot.second) {
+                                if (debug) println("$indent> skip making clay robot")
+                                false
+                            } else {
+                                true
+                            }
+                        }
+                        Robot.OBSIDIAN -> {
+                            // Don't make new obsidian robots if we have enough robots to make geodes
+                            if (robots.obsidian >= bp.geodeRobot.second) {
+                                if (debug) println("$indent> skip making obsidian robot")
+                                false
+                            } else {
+                                true
+                            }
+                        }
+                        else -> true
+                    }
                 }
-            }
-
-            fun buyAsManyAsCanAfford(robot: Robot): Store {
-                var tempStore = this
-                while (tempStore.canAfford(robot)) {
-                    tempStore = tempStore.buyRobot(robot)
+                .also { if (debug) println("$indent> Make robots $it") }
+                .maxOfOrNull { (robotType, timeToAffordRobot) ->
+                    if (debug) println("$indent> make new robot $robotType, $timeToAffordRobot")
+                    val (updatedRobots, updatedStore) = robots.makeRobot(robots.mine(store, timeToAffordRobot), robotType, bp)
+                    if (t + timeToAffordRobot >= timeLimit) {
+                        return updatedStore.geodes
+                    }
+                    dp(t + timeToAffordRobot + 1, updatedStore, updatedRobots)
                 }
-                return this
-            }
 
-            fun mine(): Store {
-                return this.copy(
-                    time = time + 1,
-                    ore = ore + oreRobots,
-                    clay = clay + clayRobots,
-                    obsidian = obsidian + obsidianRobots,
-                    geodes = geodes + geoRobots
-                )
-            }
-
-            fun mineWithStartingRobots(startingStore: Store): Store {
-                return this.copy(
-                    time = time + 1,
-                    ore = ore + startingStore.oreRobots,
-                    clay = clay + startingStore.clayRobots,
-                    obsidian = obsidian + startingStore.obsidianRobots,
-                    geodes = geodes + startingStore.geoRobots
-                )
+            return if (makeRobotMaxScore == null) {
+                // do nothing score
+                if (debug) println("$indent> mine without making robot")
+                val result = dp(t + 1, robots.mine(store), robots)
+                result
+            } else {
+                makeRobotMaxScore
             }
         }
 
-        val cache = mutableMapOf<Store, Int>()
+        val result = dp(1, Store(), RobotFactory())
+        println("Max geodes for blueprint $result $bp")
 
-        fun generatePurchaseOptions(store: Store): List<Store> {
-            return Robot.values().mapNotNull { if (store.canAfford(it)) store.buyRobot(it) else null }
+        return result
+    }
+
+    data class BluePrint(
+        val id: Int,
+        // ore cost
+        val oreRobot: Int,
+        // ore cost
+        val clayRobot: Int,
+        // ore cost, clay cost
+        val obsidianRobot: Pair<Int, Int>,
+        // ore cost, obsidian cost
+        val geodeRobot: Pair<Int, Int>,
+    ) {
+        fun costOf(robot: Robot) = when (robot) {
+            Robot.ORE -> Store(ore = oreRobot)
+            Robot.CLAY -> Store(ore = clayRobot)
+            Robot.OBSIDIAN -> Store(ore = obsidianRobot.first, clay = obsidianRobot.second)
+            Robot.GEODE -> Store(ore = geodeRobot.first, obsidian = geodeRobot.second)
+        }
+    }
+
+    data class Store(
+        var ore: Int = 0,
+        var clay: Int = 0,
+        var obsidian: Int = 0,
+        var geodes: Int = 0,
+    ) {
+        operator fun plus(other: Store) = Store(
+            ore + other.ore,
+            clay + other.clay,
+            obsidian + other.obsidian,
+            geodes + other.geodes,
+        )
+
+        operator fun minus(other: Store) = Store(
+            ore - other.ore,
+            clay - other.clay,
+            obsidian - other.obsidian,
+            geodes - other.geodes,
+        )
+
+        fun canAfford(cost: Store) =
+            ore >= cost.ore && clay >= cost.clay && obsidian >= cost.obsidian && geodes >= cost.geodes
+    }
+
+    data class RobotFactory(
+        var ore: Int = 1,
+        var clay: Int = 0,
+        var obsidian: Int = 0,
+        var geode: Int = 0,
+    ) {
+        fun mine(s: Store, t: Int = 1) = Store(ore * t, clay * t, obsidian * t, geode * t) + s
+
+        private fun addRobot(robot: Robot) = when (robot) {
+            Robot.ORE -> this.copy(ore = this.ore + 1)
+            Robot.CLAY -> this.copy(clay = this.clay + 1)
+            Robot.OBSIDIAN -> this.copy(obsidian = this.obsidian + 1)
+            Robot.GEODE -> this.copy(geode = this.geode + 1)
         }
 
-        fun dp(store: Store): Int {
-//            println("DP ${store.time} - DETAILS $store")
-            if (store.time > 24) {
-                return cache.getOrPut(store) { store.geodes }
+        fun makeRobot(s: Store, robot: Robot, bp: BluePrint): Pair<RobotFactory, Store> {
+            val robotCost = bp.costOf(robot)
+            if (!s.canAfford(robotCost)) {
+                throw IllegalArgumentException("Cannot afford robot $robot, cost:$robotCost, store:$s, $bp")
             }
 
-            val buyNothing = store.mine()
-
-            val purchaseOptions = generatePurchaseOptions(store).map { it.mineWithStartingRobots(store) }
-
-            val alternatives = purchaseOptions + buyNothing
-//            println("alternatives\n" + alternatives.joinToString("\n") {"\t" + it.toString()})
-
-            return alternatives.maxOf { cache.getOrPut(it) { dp(it) } }
+            return this.addRobot(robot) to mine(s - robotCost)
         }
 
-
-//        var store = Store()
-//
-//        repeat(24) {
-//            println("round $it")
-//            store = store
-//                .buyAsManyAsCanAfford(Robot.GEODE)
-//                .buyAsManyAsCanAfford(Robot.OBSIDIAN)
-//                .buyAsManyAsCanAfford(Robot.CLAY)
-//                .buyAsManyAsCanAfford(Robot.ORE)
-//                .mineWithStartingRobots(store)
-//        }
-
-//        return store.geodes
-//        return dp(Store(23, 5, 37, 6, 7, 1, 4, 2, 2))
-        return dp(Store())
+        fun timeToAfford(robot: Robot, store: Store, bp: BluePrint): Int? {
+            val cost = bp.costOf(robot)
+            if (store.canAfford(cost)) {
+                return 0
+            }
+            return when (robot) {
+                Robot.ORE -> {
+                    if (ore == 0) null
+                    else ceil((cost.ore - store.ore) / ore.toDouble())
+                }
+                Robot.CLAY -> {
+                    if (ore == 0) null
+                    else ceil((cost.ore - store.ore) / ore.toDouble())
+                }
+                Robot.OBSIDIAN -> {
+                    if (ore == 0 || clay == 0) null
+                    else {
+                        maxOf(
+                            ceil((cost.ore - store.ore) / ore.toDouble()),
+                            ceil((cost.clay - store.clay) / clay.toDouble())
+                        )
+                    }
+                }
+                Robot.GEODE -> {
+                    if (ore == 0 || obsidian == 0) null
+                    else {
+                        maxOf(
+                            ceil((cost.ore - store.ore) / ore.toDouble()),
+                            ceil((cost.obsidian - store.obsidian) / obsidian.toDouble())
+                        )
+                    }
+                }
+            }?.toInt()
+        }
     }
 
     enum class Robot {
         ORE,
         CLAY,
         OBSIDIAN,
-        GEODE;
+        GEODE,
     }
 }
